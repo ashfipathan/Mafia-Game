2bfc2baae5fee129a5101129941dfd6b1c20d982
Ashfi <ashfipathan@gmail.com>
2020-05-17 13:46:13 -0400

    Deleted old hierarchy

--- mafiaV2/server/routes/testAPI.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/:test", function(req, res, next) {
-    console.log(req.params.test)
-    res.send("API is working");
-});
-
-module.exports = router;
